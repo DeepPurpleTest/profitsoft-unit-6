4cebfd6caa6aa512ff914796e24c7134d9bcfe9c
Moksem <maks.viskovatov@gmail.com>
2024-06-12 18:06:36 +0300

    init

---
+++ src/main/java/org/example/profitsoftunit6gateway/ProfitsoftUnit6GatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.profitsoftunit6gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitsoftUnit6GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfitsoftUnit6GatewayApplication.class, args);
+	}
+
+}
