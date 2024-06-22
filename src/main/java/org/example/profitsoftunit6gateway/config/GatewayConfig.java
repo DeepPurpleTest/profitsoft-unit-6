8bcd66d0201c104b48d3e4df422185dce633750d
Moksem <maks.viskovatov@gmail.com>
2024-06-22 17:08:44 +0300

    refactor gateway

---
+++ src/main/java/org/example/profitsoftunit6gateway/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package org.example.profitsoftunit6gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class GatewayConfig {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("service-profitsoft-unit-2",
+						r -> r
+								.path("/api/**")
+								.filters(f -> f
+										.rewritePath("/api/(?<remaining>.*)", "/api/${remaining}"))
+								.uri("lb://profitsoft-unit-2"))
+				.build();
+	}
+}
